ce358362a6a6322e5ee7ce1c3644732a170c08b2
MrBarskih <59989968+MrBarskih@users.noreply.github.com>
2020-02-10 23:54:31 +0700

    1

--- Project/src/routes/public/bookmarks.js
+++ Project/src/routes/public/bookmarks.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 import models from '../../models';
 import http from "../../../config/http";
+import uuidv4 from 'uuid/v4'
 
 const router = Router();
 
@@ -10,8 +11,23 @@ router.get("/", (req, res) => {
   try{
 
   }catch (error) {
+
     res.status(400).json({ errors: { backend: ["Error has occured: ", error] } })
   }
 });
 
+router.post("/", async (req, res) => {
+  try{
+ 	 let channel = await models.channel.create({
+ 	 	accountId: 1,
+      	guid: uuidv4(),
+		link: req.body.link,
+		createdAt: new Date(),
+		description: req.body.description,
+		favorites: req.body.favorites
+	});
+  }catch (error) {
+  	res.status(400).json({ errors: { backend: ["Error has occured: ", error] } })
+  }
+});
 export default router;
